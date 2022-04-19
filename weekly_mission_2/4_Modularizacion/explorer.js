4eea1dea5db7716dee0dabc77d2d3ff451aace5b
josefl <jose.fl_@hotmail.com>
2022-04-19 00:52:37 -0500

    ejemplos de modularizacion

---
+++ weekly_mission_2/4_Modularizacion/explorer.js
@@ -0,0 +1,12 @@
+// Definicion de clase
+export default class Explorer{
+    constructor(name, username, mission) {
+        this.name = name
+        this.username = username
+        this.mission = mission
+    }
+  
+    getNameAndUsername() {
+        return `Explorer ${this. name}, username: ${this.username}`
+    }
+}
